7b111c412ea37ca6181d6996426d3b0fa7dbe3a8
yken88 <65884316+yken88@users.noreply.github.com>
2021-10-02 21:13:11 +0900

    タスク一覧表示, authのミドルウェアの設定

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,13 +20,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/todo', [TodoController::class, 'index'])->name('todo.index')->middleware('auth');
