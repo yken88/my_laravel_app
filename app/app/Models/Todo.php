2031b502b3afbea9ec3ab57f170e31b3538bf3d2
yken88 <65884316+yken88@users.noreply.github.com>
2021-10-06 08:09:20 +0900

    Todoクラスの±性の設定

--- app/app/Models/Todo.php
+++ app/app/Models/Todo.php
@@ -9,6 +9,11 @@ class Todo extends Model
 {
     use HasFactory;
 
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+    
     public function user() {
         return $this->belongsTo(User::class);
     }
